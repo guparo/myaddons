3e10debb5215a525782b84a1786d09e8927e7231
GuPaRo <guparo@hotmail.com>
2015-11-08 18:55:17 -0500

    del 11-8-2015

--- addons/openacademy/models.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
- 
-from openerp import models, fields, api
- 
-class openacademy_curso(models.Model):
-    _name = 'openacademy.curso'
-    name = fields.Char(string="Title", required=True)
-    description = fields.Text()
